16516effc4ef2c03a32791cbca27378f499eaa77
Lucas Vieira de Souza <lucasvs@outlook.com>
2013-09-07 21:49:31 -0300

    UPDATE: Function move box working

--- main.rb
+++ main.rb
@@ -27,29 +27,46 @@ class Game
    def move(direction)
    	case direction
    	when 'a'
-   		clear(@coordinates[0],@coordinates[1]-1)
+   		clear(@coordinates[0],@coordinates[1],'left')
    	when 'd'
-   		clear(@coordinates[0],@coordinates[1]+1)
+   		clear(@coordinates[0],@coordinates[1],'right')
    	when 's'
-   		clear(@coordinates[0]+1,@coordinates[1])
+   		clear(@coordinates[0],@coordinates[1],'down')
    	when 'w'
-   		clear(@coordinates[0]-1,@coordinates[1])
+   		clear(@coordinates[0],@coordinates[1],'up')
    	end
 
      	render()
    end
 
-   def clear(x,y)
-   	if @map[x][y] == '#'
+   def clear(x,y,pos)
+      
+   	case pos
+   	when 'left'
+   		two = y - 1
+   		one = x
+   	when 'right'
+   		two = y + 1
+   		one = x
+   	when 'down'
+   		one = x + 1
+   		two = y
+   	when 'up'
+   		one = x - 1
+   		two = y
+   	end 
+
+
+   	if @map[one][two] == '#'
    		return false
    	end
-   	if @map[x][y] == ' '
-   		return move_space(x,y)
+   	if @map[one][two] == ' '
+   		return move_space(x,y,pos)
    	end
-   	if @map[x][y] == 'o'
-   		return move_box
+   	if @map[one][two] == 'o'
+   		return move_box(one,two,pos)
    	end
-   if @map[x][y] == '.'
+   if @map[one][two] == '.'
    		return move_storage
    	end
       	
@@ -61,15 +78,43 @@ class Game
    	puts @map
    end
 
-   def move_space(x,y)   		
-   	  @map[x][y] = '@'
+   def move_space(x,y,pos)  
+   	case pos
+   	when 'left'
+   		two = y - 1
+   		one = x
+   	when 'right'
+   		two = y + 1
+   		one = x
+   	when 'down'
+   		one = x + 1
+   		two = y
+   	when 'up'
+   		one = x - 1
+   		two = y
+   	end 
+
+   if  @map[x][y] == '@'
+   	  @map[one][two] = '@'
    	  @map[@coordinates[0]][@coordinates[1]] = ' '
-   	  @coordinates[0] = x
-   	  @coordinates[1] = y
+   	  @coordinates[0] = one
+   	  @coordinates[1] = two
+   	  return true
+   end 
+
+     if  @map[x][y] == 'o' 
+   	  @map[one][two] = 'o'
+   	  @map[x][y] = ' '
+   	  return true
+   end		
+
    end
    
-   def move_box
-   	return true
+   def move_box(x,y,pos)
+   	  if clear(x,y,pos)
+   	  	clear(@coordinates[0],@coordinates[1],pos)
+   	  	return true
+   	  end
    end
 
    def move_storage
@@ -93,6 +138,7 @@ sokoban = Game.new(array)
 
 VALID_DIRECTION = %w( w s a d )
 puts 'Press W,A,S,D to move and "q" to quit'
+sokoban.move(' ')
 loop do  
   system("stty raw -echo")
   char = STDIN.read_nonblock(1) rescue nil
@@ -103,7 +149,7 @@ when *VALID_DIRECTION
   sokoban.move(char)
 else  
 end
-  sleep(1)
+  #sleep(1)
 end
 
 
